0343b436265829eb11f219148dbc2f98c6f48d22
cannotilever <cannotilever@protonmail.com>
2022-11-18 00:02:36 -0500

    bad version, only pushing because my SSD is dying. will fix in morning

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,38 @@ fn main() {
     loop {
         match iter.next() {
             Ok((packet, addr)) => {
+                // -----------Forward Packet--------------
+
                 // Allocate enough space for a new packet
                 let mut vec: Vec<u8> = vec![0; packet.packet().len()];
                 let mut new_packet = MutableUdpPacket::new(&mut vec[..]).unwrap();
-
                 // Create a clone of the original packet
                 new_packet.clone_from(&packet);
-
                 // Send the OG packet
                 match tx.send_to(new_packet, addr) {
                     Ok(n) => assert_eq!(n, packet.packet().len()),
                     Err(e) => log::error!("Couldn't forward packet due to {}",e),
                 }
+
+                // ----------Tomfoolery----------
+
+                let content = packet.payload().to_owned();
+                //version, padding, and extension, all of which don't matter to us
+                let vpe = &content[0..3];
+                // CRSC Counter, should be 0
+                let cc = &content[4..7];
+                //marker and payload type
+                let mp = &content[8..15];
+                //sequence number
+                let seq = &content[16..31];
+                //timestamp
+                let timest = &content[32..63];
+                // sync source identifiers
+                let ssrc = &content[64..95];
+                //data
+                let data = &content[96+32*cc..];
+
+
             }
             Err(e) => {
                 log::error!("Failed to read packet due to {}",e)
