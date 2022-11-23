63aad0f1aebe5576c8639a54c9c01ed82245db93
cannotilever <cannotilever@protonmail.com>
2022-11-23 12:17:42 -0500

    feat: add skeleton for packet logic

--- conversation.py
+++ conversation.py
@@ -1,6 +1,52 @@
+from scapy.all import * #just to supress IDE warnings
+import logging
+
 class conversation:
     packets = []
-    def __init__(self, src, dst):
-        self.src = src
-        self.dst = dst
+    def __init__(self, pkt: Packet):
+        self.src = pkt["IP"].src
+        self.dst = pkt["IP"].dst
         self.enforce = False
+        self.packets = []
+        match pkt["UDP"].payload.name:
+            case "RTP":
+                logging.warn("new conversation without SIP Hello!")
+            case "SIP":
+                #TODO: sippy stuff
+                print("Not yet implimented")
+
+
+    def get_enforce(self, pkt: Packet["UDP"]):
+        '''Checks if conversation should be manipulated
+        Returns: 0-packet does not correspond to this conversation
+        1- packet corresponds and conversation is not enforcing
+        2- packet corresponds and should be enforced'''
+        try:
+            ip1 = pkt["IP"].src
+            ip2 = pkt["IP"].dst
+        except IndexError:
+            logging.warning("Analysis thread: got bad packet!")
+            return 0
+        if (self.src == ip1 and self.dst == ip2) or (self.src == ip2 and self.dst == ip1):
+            if self.enforce:
+                return 2
+            return 1
+    
+    def add(self, pkt: Packet):
+        '''parses packet content and add to memory'''
+        content = pkt.lastlayer()
+        match content.name:
+            case "SIP" | "Raw":
+                self.parse_sip(content)
+            case "RTP":
+                self.parse_rtp(content)
+            case _:
+                logging.error("attempting to add invalid packet")
+
+    def parse_sip(self, content):
+        # we pretty much only have to look for SIP goodbye
+        print("Not yet implimented")
+                
+    def parse_rtp(self, content: scapy.layers.rtp.RTP):
+        # Convert RTP payload to linear sound
+        print("Not yet implimented")
--- crunch.py
+++ crunch.py
@@ -18,7 +18,9 @@ ch3 = Queue()
 def girlboss(pkt: Packet):
     global ch1, ch2
     '''gatekeep, gaslight, girlboss'''
-
+    # Packet isn't VOIP, we don't care about it
+    if pkt.lastlayer().name != "SIP" and pkt.lastlayer().name != "RTP":
+                return True
     src = pkt["IP"].src
     dst = pkt["IP"].dst
     # send full packet to analysis thread
@@ -37,15 +39,39 @@ def girlboss(pkt: Packet):
 def analysis(channel1: Queue, channel2: Queue):
     logging.info("starting analysis thread")
     conversations = []
+    
     while True:
+        # Get packets from gatekeeper
+        tmp = None
         try:
             tmp = channel1.get(block=True, timeout=0.05)
         except Empty:
             continue
-            
+        # Add new packet to list if there is one
+        if tmp is not None:
+            new = True
+            # Check if this is part of an existing sessionRTP
+            for con in conversations:
+                stat = con.get_enforce(tmp)
+                if stat == 1:
+                    # Add the packet (UDP layer and above) to conversation object
+                    con.add(tmp["UDP"])
+                    # Tell main thread to let the packet through
+                    channel2.put([tmp.src, tmp.dst, False])
+                    new = False
+                    break
+                elif stat == 2:
+                    #we're already enforcing, don't bother adding more packet data
+                    #Tell main thread to manipulate data
+                    channel2.put([tmp.src, tmp.dst, True])
+                    new = False
+                    break
+            if new:
+                conversations.append(conversation(tmp))
 
 
 
-
+# Made up NIC names for now
+## haha get it like nicknames but it's NICs i'm so funny
 packetlog = scapy.sendrecv.bridge_and_sniff(if1='enp5s0',if2='enp5s1',xfrm12=girlboss)
 packetlog.show()
